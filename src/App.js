0174cd36f784f395d1ae4ee406991fd16699135c
NilouferS <niloufer.shaikh11@gmail.com>
2019-06-04 12:04:13 +0100

    Task 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import one  from './images/avengers.jpg';
-import two from './images/fast.jpg';
-import three from './images/harry.jpg';
-import four from './images/twlight.jpg';
-import five from './images/nemo.jpg';
-import six from './images/purge.jpg';
+import avenger from './images/avengers.jpg';
+import fast from './images/fast.jpg';
+import harry from './images/harry.jpg';
+import twlight from './images/twlight.jpg';
+import nemo from './images/nemo.jpg';
+import purge from './images/purge.jpg';
 import './App.css';
 function App() {
   return (
@@ -14,14 +14,24 @@ function App() {
           KODFLIX
         </h1>
       <div className="Container">
-          <div className="item"><img src = {purge}  alt ='twlight'></img></div>
-          <div className="item"><img src = {avenger}   alt ='twlight'></img></div> 
-          <div className="item"><img src = {harry} alt ='twlight'></img> </div> 
+          <div className="item"><img src = {purge}  alt = 'The Purge'></img></div>
+          <div className="item"><img src = {avenger}   alt = 'Avengers'></img></div> 
+          <div className="item"><img src = {harry} alt = 'Harry Potter '></img> </div> 
       </div>
       <div className="Container">
-          <div className="item"><img src = {nemo} alt ='twlight'></img></div>
-          <div className="item"><img src = {twlight} alt ='twlight'></img> </div>
-          <div className="item"><img src = {fast}  alt ='twlight'></img></div>
+          <div className="item"><img src = {nemo} alt = 'Finding Nemo'></img></div>
+          <div className="item"><img src = {twlight} alt = 'Twlight'></img> </div>
+          <div className="item"><img src = {fast}  alt = 'Fast and Furious'></img></div>
+      </div>
+      <div className="Container">
+          <div className="item"><h1>Black Mirror</h1></div>
+          <div className="item"><h1>Death Note</h1></div> 
+          <div className="item"><h1>Breaking Bad</h1> </div> 
+      </div>
+      <div className="Container">
+          <div className="item"><h1>The Walking Dead </h1></div>
+          <div className="item"><h1>The Wire</h1> </div>
+          <div className="item"><h1>Games of Thrones</h1></div>
       </div>
         
       </header>
