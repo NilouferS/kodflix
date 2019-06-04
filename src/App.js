0d82cac2bc88cf0462cc1270b7caef899863a7c3
NilouferS <niloufer.shaikh11@gmail.com>
2019-06-04 16:15:18 +0100

    Task 5

--- src/App.js
+++ src/App.js
@@ -14,14 +14,38 @@ function App() {
           KODFLIX
         </h1>
       <div className="Container">
-          <div className="item"><img src = {purge}  alt = 'The Purge'></img></div>
-          <div className="item"><img src = {avenger}   alt = 'Avengers'></img></div> 
-          <div className="item"><img src = {harry} alt = 'Harry Potter '></img> </div> 
+          <div className="item"><img src = {purge}  alt = 'The Purge'></img>
+            <div className="overlay">
+                <h1>The Purge</h1>
+            </div>
+          </div>
+          <div className="item"><img src = {avenger}   alt = 'Avengers'></img>
+             <div className="overlay">
+                <h1>Avengers</h1>
+            </div>
+          </div> 
+          <div className="item"><img src = {harry} alt = 'Harry Potter '></img> 
+            <div className="overlay">
+                <h1>Harry Potter</h1>
+            </div>
+          </div> 
       </div>
       <div className="Container">
-          <div className="item"><img src = {nemo} alt = 'Finding Nemo'></img></div>
-          <div className="item"><img src = {twlight} alt = 'Twlight'></img> </div>
-          <div className="item"><img src = {fast}  alt = 'Fast and Furious'></img></div>
+          <div className="item"><img src = {nemo} alt = 'Finding Nemo'></img>
+            <div className="overlay">
+                <h1>Finding Nemo</h1>
+            </div>
+          </div>
+          <div className="item"><img src = {twlight} alt = 'Twlight'></img> 
+            <div className="overlay">
+                <h1>Twlight</h1>
+            </div>
+          </div>
+          <div className="item"><img src = {fast}  alt = 'Fast and Furious'></img>
+           <div className="overlay">
+                <h1>Fast and Furious</h1>
+            </div>
+          </div>
       </div>
       </header>
     </div>
