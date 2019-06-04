029992d7b9845d0bd280fc1d9212d3e65ef35de9
NilouferS <niloufer.shaikh11@gmail.com>
2019-06-04 11:55:11 +0100

    task 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import logo from './light.jpg';
+import one  from './images/avengers.jpg';
+import two from './images/fast.jpg';
+import three from './images/harry.jpg';
+import four from './images/twlight.jpg';
+import five from './images/nemo.jpg';
+import six from './images/purge.jpg';
 import './App.css';
 function App() {
   return (
@@ -8,11 +13,22 @@ function App() {
         <h1>
           KODFLIX
         </h1>
-        <img src = {logo} alt ='twlight'></img>
+      <div className="Container">
+          <div className="item"><img src = {purge}  alt ='twlight'></img></div>
+          <div className="item"><img src = {avenger}   alt ='twlight'></img></div> 
+          <div className="item"><img src = {harry} alt ='twlight'></img> </div> 
+      </div>
+      <div className="Container">
+          <div className="item"><img src = {nemo} alt ='twlight'></img></div>
+          <div className="item"><img src = {twlight} alt ='twlight'></img> </div>
+          <div className="item"><img src = {fast}  alt ='twlight'></img></div>
+      </div>
         
       </header>
     </div>
-  );
+     );
+    
+ 
 }
 
 export default App;
