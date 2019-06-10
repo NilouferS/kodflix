cad9c6b52dc2009c37fa1df672282aa096cca3fb
NilouferS <niloufer.shaikh11@gmail.com>
2019-06-10 15:15:48 +0100

    Task 9: Gallery Component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React from 'react';
-import avenger from './images/avengers.jpg';
-import fast from './images/fast.jpg';
-import harry from './images/harry.jpg';
-import twlight from './images/twlight.jpg';
-import nemo from './images/nemo.jpg';
-import purge from './images/purge.jpg';
-import './App.css';
-import Movie from './Movie.js';
+import MainMov from '/MainMov.js';
 
 class App extends React.Component {
   render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          KODFLIX
-        </h1>
-      <div className="Container">
-          <Movie name='The Purge' logo={ purge } />
-          <Movie name='Avengers' logo={ avenger} />
-          <Movie name='Harry Potter' logo={ harry } />
-      </div>
-      <div className="Container">
-          <Movie name='Finding Nemo' logo={ nemo } />
-          <Movie name='Twlight' logo={ twlight} />
-          <Movie name='Fast and Furious' logo={ fast } />
-      </div>
-      </header>
-      
+      <MainMov />
     </div>
      );
 }
