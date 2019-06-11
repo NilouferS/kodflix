92e918ff74baa8438d02379b34589ba6dcb158a5
NilouferS <niloufer.shaikh11@gmail.com>
2019-06-11 16:39:42 +0100

    Task 13:State Component

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Details() {
-    return(
+export default class Details extends Component {
+  constructor() {
+    super();
+    this.state = {
+      welcomeMessage: 'Hello, this will be the details page for each Movie & TV show :)'
+    }
+  }
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({
+        welcomeMessage: 'Coming Soon!'
+      });
+    },3000);
+  }
+
+  render() {
+    return (
       <div>
-        <h1>Hello, this will be the details page for each Movie & TV show :)</h1> 
+        <h1>{this.state.welcomeMessage}</h1>
         <Link to='/'>Go To Home</Link>
       </div>
     );
   }
+}
+
