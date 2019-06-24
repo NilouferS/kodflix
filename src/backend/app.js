08b6c37d63f1774db1b61f8f76da8ba26c0a461b
NilouferS <niloufer.shaikh11@gmail.com>
2019-06-24 15:47:37 +0100

    Task 19: Return a list of shows from backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const getShows = require('./shows');
+const port = 3001;
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/rest/shows', (req, res) => res.send(getShows()));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
